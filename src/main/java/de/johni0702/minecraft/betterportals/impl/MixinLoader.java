8db17c1db67ec3c06eee5bc416dff05d9958d500
embeddedt <42941056+embeddedt@users.noreply.github.com>
2022-04-19 11:35:59 -0400

    Fixes for running outside dev

---
+++ src/main/java/de/johni0702/minecraft/betterportals/impl/MixinLoader.java
@@ -0,0 +1,44 @@
+package de.johni0702.minecraft.betterportals.impl;
+
+import com.google.common.collect.ImmutableList;
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import org.jetbrains.annotations.Nullable;
+import zone.rong.mixinbooter.IEarlyMixinLoader;
+
+import java.util.List;
+import java.util.Map;
+
+@IFMLLoadingPlugin.MCVersion("1.12.2")
+public class MixinLoader implements IFMLLoadingPlugin, IEarlyMixinLoader {
+
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[0];
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> map) {
+
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+
+    @Override
+    public List<String> getMixinConfigs() {
+        return ImmutableList.of("mixins.betterportals.json", "mixins.betterportals.view.json", "mixins.betterportals.transition.json");
+    }
+}
